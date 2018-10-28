7e6a8b06248470749684550aa4c19d0ce08d7d74
harish vallabhaneni <harish.vallabhaneni9@gmail.com>
2018-10-28 01:32:57 +0000

    added support for dynamic infra setup based on env

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -1,13 +1,13 @@
 module "image" {
   source = "./image"
-  ghost_image = "${var.ghost_image}"
+  ghost_image = "${lookup(var.ghost_image, var.env)}"
 }
 
 module "container" {
   source = "./container"
   ghost_container_image = "${module.image.ghost_image_id}"
-  ghost_container_name = "${var.ghost_container_name}"
-  ghost_container_internal_port = "${var.ghost_container_internal_port}"
-  ghost_container_external_port = "${var.ghost_container_external_port}"
+  ghost_container_name = "${lookup(var.ghost_container_name, var.env)}"
+  ghost_container_internal_port = "${lookup(var.ghost_container_internal_port, var.env)}"
+  ghost_container_external_port = "${lookup(var.ghost_container_external_port, var.env)}"
 } 
 
